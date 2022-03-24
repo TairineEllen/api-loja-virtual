0f313a6ae556fa203317541f8d3791090cf71a64
TairineEllen <tairine.ellen@gmail.com>
2022-03-24 19:25:29 -0300

    add tests

---
+++ test/integration/routes/products_spec.js
@@ -0,0 +1,18 @@
+import { request } from "express";
+
+describe('Routes: Products', () => {
+  const defaultProduct = {
+    name: 'Default Product',
+    description: 'Product description',
+    price: 100
+  };
+
+  describe('GET /products', () => {
+    it('should return a list of products', done => {
+      request.get('/products').end((err, res) => {
+        expect(res.body[0]).to.eq(defaultProduct);
+        done(err);
+      });
+    });
+  });
+});
