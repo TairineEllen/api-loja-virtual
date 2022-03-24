286065c69333a8dc0390de1e6857f5a1b2ab01b2
TairineEllen <tairine.ellen@gmail.com>
2022-03-24 19:34:27 -0300

    implement green stage

--- src/app.js
+++ src/app.js
@@ -6,5 +6,10 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Hello, World'));
+app.get('/products', (req, res) => res.send([{
+  name: 'Default Product',
+  description: 'Product description',
+  price: 100
+}]));
 
 export default app;
